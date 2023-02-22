06d3bd7e76886ce8ca4fca9e4d9b59e62b99502f
Martonnvok <115223813+Martonnvok@users.noreply.github.com>
2023-02-22 12:41:37 +0100

    Előzők javítása, 4-es feladat (félkész)

--- elozoOrai.java
+++ elozoOrai.java
@@ -3,22 +3,30 @@
 
 public class elozoOrai {
     public static void main(String[] args) {
-//       int osszeg=elso10SzamOsszege();
-//       String s = String.format("Az első 10 szám összege: %d", osszeg );
-//       kiir(s+"\n");
-//       
-//       int a = 7, b = 14;
-//        osszeg = ossze(a, b);
-//        s = String.format("%d + %d = %d\n", a, b, a + b);
-//        kiir(s);
+       int osszeg=elso10SzamOsszege();
+       String s = String.format("Az első 10 szám összege: %d", osszeg );
+       kiir(s+"\n");
+       
+       int a = 7, b = 14;
+        osszeg = ossze(a, b);
+        s = String.format("%d + %d = %d\n", a, b, a + b);
+        kiir(s);
 
+        negySzamOssz();
+        
+       int egy=1, ketto=2, harom=3;
+       String s3 = String.format("%d + %d + %d = %d\n", egy, ketto, harom, Math.pow(egy, ketto) );
+       kiir(s3+"\n");
+
+    }
+
+    private static void negySzamOssz() {
         int osszeg4 = 0;
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 5; i++) {
             osszeg4 += i;
         }
         String s2 = String.format("Az első 4 szám összege: %d", osszeg4 );
         kiir(s2+"\n");
-
     }
 
     
