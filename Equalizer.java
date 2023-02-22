8f486b347e54125636f47dc8647cd7054773dd0c
Martonnvok <115223813+Martonnvok@users.noreply.github.com>
2023-02-22 13:33:44 +0100

    metódus

--- Equalizer.java
+++ Equalizer.java
@@ -4,24 +4,29 @@ import java.util.Random;
 public class Equalizer {
     public static void main(String[] args) {
         Random r = new Random();
+        String s = String.format("\u001B[45m" );
         for (int i = 0; i < 5; i++) {
             int szam = r.nextInt(3,7);
             if (szam == 3) {
-                System.out.println("\u001B[45m" + " "+ " "+ " ");
+                kiir(s+"   ");
             }
             else if (szam == 4) {
-                System.out.println("\u001B[45m" + " "+ " "+ " "+ " ");
+                kiir(s+"    ");
             }
             else if (szam == 5) {
-                System.out.println("\u001B[45m" + " "+ " "+ " "+ " "+ " ");
+                kiir(s+"     ");
             }
             else if (szam == 6) {
-               System.out.println("\u001B[45m" + " "+ " "+ " "+ " "+ " "+ " "); 
+               kiir(s+"      ");
             }
             else if (szam == 7) {
-               System.out.println("\u001B[45m" + " "+ " "+ " "+ " "+ " "+ " "+ " "); 
+               kiir(s+"       ");
             }
             
         }
+    }
+
+    private static void kiir(String str) {
+        System.out.println(str);
     }
 }
